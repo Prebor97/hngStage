e5b0d4ec9616638b4bb8d8d84b4993a1344e710f
Prebor97 <prebstamar@gmail.com>
2025-01-31 15:06:18 -0800

    commit

--- src/main/java/com/example/hngStage/HngService.java
+++ src/main/java/com/example/hngStage/HngService.java
@@ -4,17 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class HngService {
 
-    @Autowired
-    private DateUtil dateUtil;
-
     public ResponseDto getInfo(){
         LocalDateTime date = LocalDateTime.now();
-        String presentDate = DateUtil.formatUsingDateTimeFormatter(date);
+        String presentDate = this.formatUsingDateTimeFormatter(date);
         return new ResponseDto("prebstamar@gmail.com",presentDate,"https://github.com/Prebor97/hngStage");
+    }
+
+    public String formatUsingDateTimeFormatter(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+        return localDateTime.atOffset(ZoneOffset.UTC).format(formatter);
     }
 
 }
