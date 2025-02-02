0b67ff5ec00c103b45aeaf308072d5dd19c7dceb
Prebor97 <prebstamar@gmail.com>
2025-02-01 22:19:14 -0800

    fjfj

--- src/main/java/com/example/hngStage/HngController.java
+++ src/main/java/com/example/hngStage/HngController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
+@CrossOrigin(origins = "https://hngstage-production.up.railway.app/", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/getInfo")
 @RequiredArgsConstructor
