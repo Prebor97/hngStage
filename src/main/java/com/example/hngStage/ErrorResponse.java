f644e9ce4b6d7ff7487a2c747e7938277b254362
Prebor97 <prebstamar@gmail.com>
2025-02-06 09:04:44 -0800

    m

---
+++ src/main/java/com/example/hngStage/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.example.hngStage;
+
+public class ErrorResponse {
+
+    public String number;
+    public boolean error;
+
+    public ErrorResponse(String number, boolean error) {
+        this.number = number;
+        this.error = error;
+    }
+}
